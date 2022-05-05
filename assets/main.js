88a3f72d9fe0601414022186e38c98bdc0be9e14
Juanjo CGB <jj.alonso@esla.com>
2022-05-06 00:48:50 +0200

    Refactorización del código

--- assets/main.js
+++ assets/main.js
@@ -7,8 +7,6 @@
  "use strict";
 
 const range = document.querySelector('.encuesta__input-range');
-const containerEmojis = document.querySelector('.encuesta__emoji');
-const containerValores = document.querySelector('.encuesta__valores');
 const resultado = document.querySelector('.encuesta__resultado');
 const nota = document.querySelector('.encuesta__nota');
 /**
@@ -20,7 +18,7 @@ const valoraciones = {
     regular: 'rgba(255,111,0,1)',
     aceptable: 'rgba(255,168,0,1)',
     excelente: 'rgba(58,181,164,1)'
-}
+};
 /**
   * Establece el año en el footer
   * @returns {void}
@@ -39,7 +37,7 @@ function obtenerKeyValoraciones (indice) {
     let contador = 0;
     for (let key in valoraciones) { 
         if (contador === indice){
-            return key
+            return key;
         }
         contador ++;
     }
@@ -59,75 +57,59 @@ if (range) {
         range.style.setProperty("--value", range.value);
         //Cambiamos el emoji
         if(navigator.userAgent.indexOf("Chrome") > -1) {
-            style.innerHTML += `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
-        } else if (sUsrAg.indexOf("Safari") > -1) {
-            style.innerHTML += `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
-        } else if (sUsrAg.indexOf("Opera") > -1) {
-            style.innerHTML += `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
-        } else if (sUsrAg.indexOf("Firefox") > -1) {
-            style.innerHTML += `input[type=range]::-moz-range-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
-        } else if (sUsrAg.indexOf("MSIE") > -1) {
-            style.innerHTML += `input[type=range]::-ms-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
+            style.innerHTML = `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
+        } else if (navigator.userAgent.indexOf("Safari") > -1) {
+            style.innerHTML = `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
+        } else if (navigator.userAgent.indexOf("Opera") > -1) {
+            style.innerHTML = `input[type=range]::-webkit-slider-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
+        } else if (navigator.userAgent.indexOf("Firefox") > -1) {
+            style.innerHTML = `input[type=range]::-moz-range-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
+        } else if (navigator.userAgent.indexOf("MSIE") > -1) {
+            style.innerHTML = `input[type=range]::-ms-thumb {background-image: url(../assets/imgs/${String(rangeValue)}.png);}`;
         }
         if (rangeValue <= 2) {
-            if(navigator.userAgent.indexOf("Chrome") > -1) {
+            if (navigator.userAgent.indexOf("Firefox") > -1) {
+                style.innerHTML += `input[type=range]::-moz-range-progress {background-color: ${valoraciones.deficiente}; height: 12px;}`;
+            } else if (navigator.userAgent.indexOf("MSIE") > -1) {
+                style.innerHTML += `input[type=range]::-ms-fill-lower {background-color: ${valoraciones.deficiente}; height: 12px;}`;
+            } else {
                 style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.deficiente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Safari") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.deficiente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Opera") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.deficiente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Firefox") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-moz-range-track {background-image:linear-gradient(to right, ${valoraciones.deficiente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("MSIE") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-ms-track {background-image:linear-gradient(to right, ${valoraciones.deficiente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
             }
             //Actualizamos el resultado
             resultado.innerHTML = obtenerKeyValoraciones (0);
             resultado.style.color = valoraciones.deficiente;
             nota.innerHTML = `(${rangeValue})`;
         } else if (rangeValue > 2 && rangeValue <= 5){
-            if(navigator.userAgent.indexOf("Chrome") > -1) {
+            if (navigator.userAgent.indexOf("Firefox") > -1) {
+                style.innerHTML += `input[type=range]::-moz-range-progress {background-color: ${valoraciones.regular}; height: 12px;}`;
+            } else if (navigator.userAgent.indexOf("MSIE") > -1) {
+                style.innerHTML += `input[type=range]::-ms-fill-lower {background-color: ${valoraciones.regular}; height: 12px;}`;
+            } else {
                 style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.regular} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Safari") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.regular} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Opera") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.regular} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Firefox") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-moz-range-track {background-image:linear-gradient(to right, ${valoraciones.regular} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("MSIE") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-ms-track {background-image:linear-gradient(to right, ${valoraciones.regular} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
             }
             //Actualizamos el resultado
             resultado.innerHTML = obtenerKeyValoraciones (1);
             resultado.style.color = valoraciones.regular;
             nota.innerHTML = `(${rangeValue})`;
         } else if (rangeValue > 5 && rangeValue <= 8){
-            if(navigator.userAgent.indexOf("Chrome") > -1) {
+            if (navigator.userAgent.indexOf("Firefox") > -1) {
+                style.innerHTML += `input[type=range]::-moz-range-progress {background-color: ${valoraciones.aceptable}; height: 12px;}`;
+            } else if (navigator.userAgent.indexOf("MSIE") > -1) {
+                style.innerHTML += `input[type=range]::-ms-fill-lower {background-color: ${valoraciones.aceptable}; height: 12px;}`;
+            } else {
                 style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.aceptable} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Safari") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.aceptable} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Opera") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.aceptable} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Firefox") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-moz-range-track {background-image:linear-gradient(to right, ${valoraciones.aceptable} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("MSIE") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-ms-track {background-image:linear-gradient(to right, ${valoraciones.aceptable} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
             }
             //Actualizamos el resultado
             resultado.innerHTML = obtenerKeyValoraciones (2);
             resultado.style.color = valoraciones.aceptable;
             nota.innerHTML = `(${rangeValue})`;
         }else if (rangeValue > 8){
-            if(navigator.userAgent.indexOf("Chrome") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.excelente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Safari") > -1) {
-                style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.excelente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Opera") > -1) {
+            if (navigator.userAgent.indexOf("Firefox") > -1) {
+                style.innerHTML += `input[type=range]::-moz-range-progress {background-color: ${valoraciones.excelente}; height: 12px;}`;
+            } else if (navigator.userAgent.indexOf("MSIE") > -1) {
+                style.innerHTML += `input[type=range]::-ms-fill-lower {background-color: ${valoraciones.excelente}; height: 12px;}`;
+            } else {
                 style.innerHTML += `input[type=range]::-webkit-slider-runnable-track {background-image:linear-gradient(to right, ${valoraciones.excelente}calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("Firefox") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-moz-range-track {background-image:linear-gradient(to right, ${valoraciones.excelente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
-            } else if (sUsrAg.indexOf("MSIE") > -1) {
-                style.innerHTML += `input[type=range]::input[type=range]::-ms-track {background-image:linear-gradient(to right, ${valoraciones.excelente} calc(var(--value)*1%), var(--background-primary-color) 0);}`;
             }
             //Actualizamos el resultado
             resultado.innerHTML = obtenerKeyValoraciones (3);
